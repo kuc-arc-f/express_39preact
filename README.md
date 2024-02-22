3c1ea3d05b083a5346f3889001a2c2c3d10c525b
_ñ <knaka0209@gmail.com>
2024-02-22 21:03:00 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+﻿# express_39preact
+
+ Version: 0.9.1
+
+ Author  : Kouji Nakashima / kuc-arc-f.com
+
+ date    : 2024/02/21
+
+ update :
+
+***
+
+Preact.js + express, sample
+
+***
+### blog
+
+***
+
---
+++ src/client/About.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+console.log("#about");
+import Head from '../components/Head.svelte';
+</script>
+
+<!-- -->
+<main>
+  <div class="container mx-auto my-2 px-8 bg-white">
+    <Head />
+    <hr />
+    <h1 class="text-4xl font-bold">About</h1>
+  </div>
+</main>
+
+<style>
+</style>
+<!--
+<a href="/">[ home ]</a>
+-->
