60e40814410ba24fbe7300715df2be1b045b2dd4
_ñ <knaka0209@gmail.com>
2024-02-23 16:32:14 +0900

    up, 0223a11

--- src/client/Test/app.tsx
+++ src/client/Test/app.tsx
@@ -1,9 +1,23 @@
 import { useState } from 'preact/hooks'
 import Head from '../../components/Head';
+
+//
+const dataItems: any[] = [
+  {id:1, title: "title_1"},
+  {id:2, title: "title_2"},
+  {id:3, title: "title_3"},
+];
+let items: any[] = [];
 //
 export function App() {
-  const [count, setCount] = useState(0)
-
+  const [count, setCount] = useState(0);  
+  const [updatetime, setUpdatetime] = useState("");
+  //
+  const testProc = function(){
+    items = dataItems;
+    setUpdatetime(new Date().toString());
+ }
+  //
   return (
     <div class="container mx-auto my-2 px-8 bg-white">
       <div>
@@ -12,11 +26,18 @@ export function App() {
       <h1 class="text-4xl font-bold">Test</h1>
       <hr />
       <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          [ count ]  is {count}
+        <button class="btn-purple" onClick={() => testProc()}>Test
         </button>
       </div>
       <hr />
+      {items.map((item: any) => {
+        return (
+        <li key={item.id}>
+          <h3 className="text-3xl font-bold">{item.title}</h3>
+          <span>id: {item.id}</span>
+        </li>
+        );
+      })}
     </div>
   )
 }
